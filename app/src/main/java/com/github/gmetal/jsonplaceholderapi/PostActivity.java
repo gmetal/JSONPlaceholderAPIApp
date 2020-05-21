14408298bcc33c8f16c0d22cfae9f6a7a40a093e
George Metaxas <gmetaxas@gmail.com>
2020-05-21 11:01:32 +0300

    Implement the PostActivity (2) and add the View Comments button for opening the PostCommentsActivity

---
+++ app/src/main/java/com/github/gmetal/jsonplaceholderapi/PostActivity.java
@@ -0,0 +1,83 @@
+package com.github.gmetal.jsonplaceholderapi;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class PostActivity extends AppCompatActivity {
+
+    public static final String POST_ID_EXTRA = "post_id";
+
+    private ProgressDialog progressDialog;
+    private TextView postIdTextView;
+    private TextView userIdTextView;
+    private TextView titleTextView;
+    private TextView bodyTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
+        final int postId = getIntent().getIntExtra(POST_ID_EXTRA, 0);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading post...");
+        progressDialog.show();
+
+        postIdTextView = findViewById(R.id.postIdLabel);
+        userIdTextView = findViewById(R.id.userIdLabel);
+        titleTextView = findViewById(R.id.postTitle);
+        bodyTextView = findViewById(R.id.postBody);
+
+        final Button viewCommentsButton = findViewById(R.id.viewCommentsButton);
+        viewCommentsButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(final View v) {
+
+                Intent intent = new Intent(PostActivity.this, PostCommentsActivity.class);
+                intent.putExtra(PostCommentsActivity.POST_ID_EXTRA, postId);
+                startActivity(intent);
+            }
+        });
+
+        JSONPlaceholderService serviceInstance = RetrofitClientSingleton.getInstance().getRetrofit().create(JSONPlaceholderService.class);
+        final Call<Post> postById = serviceInstance.getPostById(postId);
+        postById.enqueue(new Callback<Post>() {
+
+            @Override
+            public void onResponse(final Call<Post> call, final Response<Post> response) {
+
+                progressDialog.dismiss();
+                final Post post = response.body();
+                updateUi(post);
+            }
+
+            @Override
+            public void onFailure(final Call<Post> call, final Throwable t) {
+
+                progressDialog.dismiss();
+                Toast.makeText(PostActivity.this, "Failed to retrieve Post by id", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void updateUi(final Post post) {
+
+        postIdTextView.setText(getString(R.string.post_id_label, post.getId()));
+        userIdTextView.setText(getString(R.string.user_id_label, post.getUserId()));
+        titleTextView.setText(getString(R.string.title_label, post.getTitle()));
+        bodyTextView.setText(getString(R.string.body_label, post.getBody()));
+    }
+}
